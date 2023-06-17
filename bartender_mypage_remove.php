5593d0503ce159e1e0b579d77bdaafcc0149c658
gloglow <db_98@naver.com>
2023-06-17 10:53:14 +0900

    Update bartender_mypage_remove.php

--- bartender_mypage_remove.php
+++ bartender_mypage_remove.php
@@ -1,6 +1,6 @@
 <?php require_once('lib/header.php'); ?>
   <?php
-  $conn = mysqli_connect("localhost", "silvestern", "siterntlxjs98*", "silvestern");
+  $conn = mysqli_connect("*", "*", "*", "*");
   $member_info = "DELETE FROM member WHERE member_id='{$_SESSION['login_id']}'";
 
   $result = mysqli_query($conn, $member_info);
